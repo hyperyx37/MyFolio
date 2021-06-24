1a08e65dca623a1136bad44d9d77e0fcdc84552f
hyperyx37 <47114480+hyperyx37@users.noreply.github.com>
2021-06-24 18:34:02 -0400

    Updates

---
+++ precache-manifest.8d031b5faf1459aed626a68635e74de6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22937353707e6b7963cef66d83c0c2c7",
+    "url": "/MyFolio/index.html"
+  },
+  {
+    "revision": "902d52254185b9c76e10",
+    "url": "/MyFolio/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "63f5c1136432e4b01bda",
+    "url": "/MyFolio/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "a40fe5ff88fe62a7b18b",
+    "url": "/MyFolio/static/css/main.9f64e7e4.chunk.css"
+  },
+  {
+    "revision": "b863223b418e23822080",
+    "url": "/MyFolio/static/js/2.8e6de723.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/MyFolio/static/js/2.8e6de723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "902d52254185b9c76e10",
+    "url": "/MyFolio/static/js/3.7f5bb110.chunk.js"
+  },
+  {
+    "revision": "63f5c1136432e4b01bda",
+    "url": "/MyFolio/static/js/4.84f00cb1.chunk.js"
+  },
+  {
+    "revision": "a40fe5ff88fe62a7b18b",
+    "url": "/MyFolio/static/js/main.6ce64607.chunk.js"
+  },
+  {
+    "revision": "7b58ff20b176dc46828a",
+    "url": "/MyFolio/static/js/runtime-main.bdaa6c29.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/MyFolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/MyFolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "5cae841e5205c1c10dc23c05449004da",
+    "url": "/MyFolio/static/media/OhioLogo.5cae841e.jpg"
+  },
+  {
+    "revision": "2b0ccea0c8c9f692004f5a89499cecd0",
+    "url": "/MyFolio/static/media/Westernlogo.2b0ccea0.jpg"
+  },
+  {
+    "revision": "5e92ece193b7328eeafae7082abb5a8f",
+    "url": "/MyFolio/static/media/airbnbLogo.5e92ece1.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/MyFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/MyFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/MyFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "4cc9fd84110d1bbc2e5b57b3bb1dad16",
+    "url": "/MyFolio/static/media/facebookLogo.4cc9fd84.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/MyFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/MyFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/MyFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/MyFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "507c3a23f87ee1560ad62c7ce8139e1b",
+    "url": "/MyFolio/static/media/quoraLogo.507c3a23.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/MyFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/MyFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/MyFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
